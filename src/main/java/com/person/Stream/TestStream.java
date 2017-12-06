10e985f04ee34d021e8000843fa0886bca8b57f5
xujf <xujf@firstshare.cn>
2017-12-06 14:21:23 +0800

    2017-12-06

---
+++ src/main/java/com/person/Stream/TestStream.java
@@ -0,0 +1,13 @@
+package com.person.Stream;
+
+/**
+ * Created by xujf on 2017/12/6.
+ */
+public class TestStream {
+
+    //    (Map)idList.stream().distinct().collect(Collectors.toMap((x) -> {
+    //        return x;
+    //    }, (x) -> {
+    //        return Permissions.READ_WRITE;
+    //    }));
+}
