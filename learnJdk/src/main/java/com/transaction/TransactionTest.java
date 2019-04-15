82c8e1ccee5fa8975a884480bd30ff331122fae2
shawnxjf1 <shawn_angel@qq.com>
2019-04-15 21:08:59 +0800

    2019-04-15

--- learnJdk/src/main/java/com/transaction/TransactionTest.java
+++ learnJdk/src/main/java/com/transaction/TransactionTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
  */
 public class TransactionTest {
 
+    /////////////// ************case 1**************//////////////////////////////
     @Test
     @Transactional(propagation = Propagation.MANDATORY)
     public void insertPerformanceMapper() {
@@ -15,11 +16,41 @@ public class TransactionTest {
     }
 
     // @Transactional
-    public void insert() { 
-        ..
+    public void insert() {
+        //
         // throw new RuntimeException("ceshi");
     }
     // 执行结果
     // org.springframework.transaction.IllegalTransactionStateException: No existing transaction found for transaction
     // marked with propagation 'mandatory'
+
+    /////////////// ************case 2**************//////////////////////////////
+    @Test
+    @Transactional(propagation = Propagation.MANDATORY)
+    public void insertPerformanceMapper() {
+        this.insert();
+    }
+
+    @Transactional
+    public void insert() {
+        //
+        // throw new RuntimeException("ceshi");
+    }
+    // 执行结果,同样会报错
+    // org.springframework.transaction.IllegalTransactionStateException: No existing transaction found for transaction
+    // marked with propagation 'mandatory'
+
+    /////////////// ************case 1**************//////////////////////////////
+    @Test
+    @Transactional
+    public void insertPerformanceMapper() {
+        this.insert();
+    }
+
+    @Transactional(propagation = Propagation.MANDATORY)
+    public void insert() {
+        //
+        // throw new RuntimeException("ceshi");
+    }
+    // 不会报错，Mandatory要求调用它的时候已经存在，比如A调用B，B的事务级别为Mandatory那么,A调用B的时候事务必须已经存在。
 }
