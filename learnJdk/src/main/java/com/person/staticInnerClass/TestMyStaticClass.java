12cd1d65d6d244c9d3bab6f4561a3d2cb6bb85c6
shawnxjf1 <shawn_angel@qq.com>
2019-04-18 09:56:45 +0800

    2019-04-18

--- learnJdk/src/main/java/com/person/staticInnerClass/TestMyStaticClass.java
+++ learnJdk/src/main/java/com/person/staticInnerClass/TestMyStaticClass.java
@@ -1,65 +1,66 @@
 package com.person.staticInnerClass;
 
-//FIXME 谢了代码一定要描述出自己的认知，不然自己还要搞一遍。<br>
-/**
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+/***
+ * 背景：<br>
+ * 功能描述： 1.FinalClass 有点像staticclass不能被new，直接使用<br>
  * 参考：http://stackoverflow.com/questions/7486012/static-classes-in-java <br>
  * 
- * @author lakala-shawn
- *
+ * @author shawn
+ * @date 2019/4/17
+ * @param
+ * @return
  */
 public class TestMyStaticClass {
 
-	public static void main(String[] args) {
-		MyStaticClass.setMyStaticMember(5);
-		System.out.println("Static value: " + MyStaticClass.getMyStaticMember());
-		System.out.println("Value squared: " + MyStaticClass.squareMyStaticMember());
-		// MyStaticClass x = new MyStaticClass(); // results in compile time
-		// error
-	}
+    @Test
+    public void testFinalClass() {
+        MyFinalClass.setMyStaticMember(5);// MyFinalClass不能被实例化直接当做静态类相似情况来使用<br>
+        // MyFinalClass x = new MyFinalClass(); // 编译报错-results in compile time error,MyStaticClass構造方法是private的
+        System.out.println("Static value: " + MyFinalClass.getMyStaticMember());
+        System.out.println("Value squared: " + MyFinalClass.squareMyStaticMember());
+        //
+    }
+
+    @Test
+    public void testStaticClass() {
+        StaticClass.staticMapExample.put("message",
+            "static class must be inner class,and the private member can be access by others");
+    }
+
+    private static class StaticClass {
+        static {
+
+        }
+        private static Map<String, String> staticMapExample = new HashMap<>();
+    }
+
 }
 
 // final class 被称做 top-level java class
 // A top-level Java class mimicking static class behavior
-final class MyStaticClass {
-	private MyStaticClass() { // private constructor
-		myStaticMember = 1;
-	}
+final class MyFinalClass {
+    private MyFinalClass() { // private constructor
+        myStaticMember = 1;
+    }
 
-	// Make all the members and functions of the class static
-	private static int myStaticMember;
+    // Make all the members and functions of the class static
+    private static int myStaticMember;
 
-	public static void setMyStaticMember(int val) {
-		myStaticMember = val;
-	}
+    public static void setMyStaticMember(int val) {
+        myStaticMember = val;
+    }
 
-	public static int getMyStaticMember() {
-		return myStaticMember;
-	}
+    public static int getMyStaticMember() {
+        return myStaticMember;
+    }
 
-	public static int squareMyStaticMember() {
-		return myStaticMember * myStaticMember;
-	}
+    public static int squareMyStaticMember() {
+        return myStaticMember * myStaticMember;
+    }
+
 }
-
-/*
- * 报错:
-//Illegal modifier for the class staticClass; only public, abstract & final are permitted
-static class staticClass
-{
-	
-}*/
-
-//JDK 中的体现:
-// 比如java.lang.Math (就是一个函数 - 函数式编程)
-// public final class Math {
-//
-// /**
-// * Don't let anyone instantiate this class.
-// */
-// private Math() {}
-//
-// /**
-// * The {@code double} value that is closer than any other to
-// * <i>e</i>, the base of the natural logarithms.
-// */
-// public static final double E = 2.7182818284590452354;
---
+++ learnJdkWithSpringBoot/src/test/java/com/person/TestSpring.java
@@ -0,0 +1,21 @@
+package com.person;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.HashMap;
+import java.util.List;
+
+//@Slf4j
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:spring-test/applicationContext.xml"} )
+public class TestSpring {
+
+
+
+
+}
